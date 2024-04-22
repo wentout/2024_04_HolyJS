13e6fc9e8f61a439e5b06096e3a9f2b6fc11ca83
went.out <went.out@gmail.com>
2024-04-22 16:43:42 +0300

    preparing gh-pages

--- slider/vite.config.ts
+++ slider/vite.config.ts
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3030,
+    strictPort: true
+  },
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 2048
+  },
   plugins: [
     react(),
   ],
